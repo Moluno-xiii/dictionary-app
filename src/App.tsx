df2303c2220d744a9b74a4574c86147ce3e75e5c
Moluno-xiii <molunoprogress@gmail.com>
2024-04-13 10:44:59 +0100

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    //  <div className="text-blue-500 py-4 flex justify-center">
+    <div className="flex justify-center bg-black px-4 py-4 text-blue-500">
+      Trace Anime App
+    </div>
+  );
+}
+
+export default App;
